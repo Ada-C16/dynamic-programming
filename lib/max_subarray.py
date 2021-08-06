79d8db4fc96eac5db38b46994cf733f21fd43214
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-06 14:14:46 -0700

    solutions

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -9,4 +9,13 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    
+    max_subarray_so_far = nums[0]
+    max_this_sub_array = nums[0]
+
+    for i in range(1, len(nums)):
+        max_this_sub_array = max(max_this_sub_array + nums[i], nums[i])
+        max_subarray_so_far = max(max_subarray_so_far, max_this_sub_array)
+
+    return max_subarray_so_far
+    
