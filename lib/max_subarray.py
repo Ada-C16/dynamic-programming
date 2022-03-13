e32887b8118c5b3a95867d7a8bdf1cf6dfc18a88
wangjiajia95 <grace.wangjiajia@gmail.com>
2022-03-12 16:52:21 -0800

    hw

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -1,4 +1,4 @@
-
+import math
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
@@ -9,4 +9,16 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    largest_sum = [0] * len(nums)
+    largest_sum[0] = nums[0]
+
+    for i in range(1, len(nums)):
+        if largest_sum[i - 1] <= 0:
+            largest_sum[i] = nums[i]
+            
+        else:
+            largest_sum[i] = nums[i] + largest_sum[i-1]
+           
+    return max(largest_sum)
+        
+
