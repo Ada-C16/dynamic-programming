99b25cd0e6a98a2f96a216671e74d91dafef5a45
Elly Wong <elly.yywong@gmail.com>
2022-04-29 22:39:35 -0700

    finished

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -2,9 +2,25 @@
 
 # Time complexity: ?
 # Space Complexity: ?
+
+    
 def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+    if num == 0:
+        raise ValueError
+    
+    # if num == 1 or num == 2:
+    #     return "1" 
+    
+    memo_arr = [0, 1, 1]
+    
+    for i in range(3, num + 1):
+        memo_arr.append(memo_arr[memo_arr[i-1]] + memo_arr[i - memo_arr[i - 1]])
+
+    res_str = ""
+    for j in range(1, num+1):    
+        res_str += str(memo_arr[j]) + " "
+        
+    res_str = res_str[:-1]
+    return res_str
+
+print(newman_conway(1))
