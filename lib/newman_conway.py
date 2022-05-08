962dc1685dad160a4f78f2685120582d5daddca5
sarahstandish <z.sisyphusrocks@gmail.com>
2022-05-08 09:04:00 -0700

    passing all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -2,9 +2,26 @@
 
 # Time complexity: ?
 # Space Complexity: ?
+from multiprocessing.sharedctypes import Value
+
+
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num < 1:
+        raise ValueError
+
+    if num == 1:
+        return str(num)
+
+    p = [0, 1, 1]
+    s = "1 1"
+
+    for n in range(3, num + 1):
+        val = p[p[n - 1]] + p[n - p[n - 1]]
+        p.append(val)
+        s += " " + str(val)
+
+    return s
