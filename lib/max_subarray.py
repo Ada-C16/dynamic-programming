3b489cd1304de49b8518dc2393f48612e9a53417
Shay <shay.phillips11@gmail.com>
2022-05-12 19:50:09 -0400

    finished

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,17 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
-    if nums == None:
+    if not nums:
         return 0
-    if len(nums) == 0:
-        return 0
-    pass
+    
+    max_so_far = nums[0]
+    max_ending_here = 0
+
+    for num in nums:
+        max_ending_here = max(num, max_ending_here + num)
+        max_so_far = max(max_so_far, max_ending_here)
+    
+    return max_so_far
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,17 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num == 0:
+        raise ValueError("Input must be greater than 0.")
+
+    if num == 1:
+        return "1"
+    
+    list = [0, 1, 1]
+    
+    for i in range(3, num + 1):
+        list.append(list[list[i-1]] + list[i-list[i-1]])
+
+    return " ".join(str(x) for x in list[1:])
