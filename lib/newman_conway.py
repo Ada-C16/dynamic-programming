1c45c2c1efbac5bc494a9f0e70202926e408ecfa
karishma <karishmacarterjohnson@gmail.com>
2022-05-12 14:13:46 -0700

    all tests pass

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,26 @@
+def stringify(nmc):
+    nmc.pop(0) # O(n)
+    nmc = [str(x) for x in nmc] # O(n)
+    return " ".join(nmc)
 
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    # P(n) = P(P(n - 1)) + P(n - P(n - 1))
+    if num == 0:
+        raise ValueError()
+    if num == 1:
+        return "1"
+    if num == 2:
+        return "1 1"
+    nmc = [None, 1, 1]
+    i = 3
+    while i <= num : # O(n)
+        nmc.append(nmc[nmc[i-1]] + nmc[i - nmc[i-1]])
+        i += 1
+
+    return stringify(nmc)
