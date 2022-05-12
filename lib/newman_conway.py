c4997ca71c20ac629c048fb8be8e557e505786cb
Michelle Bodart <michelle.bodart@gmail.com>
2022-05-12 15:34:38 -0700

    newman conway works

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,8 +3,22 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
-    """
-    pass
+    if num < 1:
+        raise(ValueError)
+    elif num == 1:
+        return '1'
+    elif num == 2:
+        return '1 1'
+
+    memo = [None] * (num + 1)
+    memo[1] = 1
+    memo[2] = 1
+
+    for i in range(3, num + 1):
+        memo[i] = memo[memo[i - 1]] + memo[i - memo[i - 1]]
+
+    result = str(memo[1])
+    for i in range(2, len(memo)):
+        result += " " + str(memo[i])
+    return result
+
