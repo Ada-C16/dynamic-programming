27afd463955b5a3af24183c5c7fb91b478f5548a
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-05-12 20:35:54 -0700

    Added big o notation

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,11 +1,7 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
     if num <= 0:
         raise ValueError
