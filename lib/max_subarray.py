0a53b3a4aa65393387386fa6a1083f52fc7d330b
Michelle Bodart <michelle.bodart@gmail.com>
2022-05-13 08:41:32 -0700

    cleanup

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -12,46 +12,22 @@ def max_sub_array(nums):
     
     max = None
     current = None
-    i = 0
-    while i < len(nums):
+
+    for i in range(len(nums)):
         
         # get next number
         num = nums[i]
 
         # update current
-        if not current:
-            current = num
-        else:
-            current += num
+        current = num if not current else current + num
 
-        # if current sum is > max, replace it
+        # if there is no max or current sum is > max, replace it
         if not max or current > max:
             max = current
 
-        # if current is negative, start over
+        # if current is negative, start over, it's not adding value to the
+        # subarray
         if current < 0:
             current = None
 
-        # move forward by one
-        i += 1
-
     return max
-
-
-        
-
-        # get the next number
-        # if it is positive, add it to current and compare to max, update if needed
-        # if it is negative, compare it to the current. If it is still positive, continue
-        # if it is negative, move both pointers up to the next positive num
-
-
-    # if a subarray of positive numbers is less than the preceeding subarray of 
-    # negative numbers, we should split it at the negative numbers because
-    # it won't be included in an array to the left, because the negative nums
-    # will more than cancel it out. But what if to the right of it there is a 
-    # negative 1 and positive 100? Then what? 
-    # new thought, if a current sum is higher than the negative group to its right,
-    # it should be included because net positive, otherwise, axe it
-    # 1  5 -3 -5 6 -1 99 3 | 107 vs 105
-    # 50 5 -3 -5 6 -1 99 3 | 107 vs 154
