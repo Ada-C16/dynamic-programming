bff63df226b267a690fe46d7d3be797c9471d0e1
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-05-12 19:47:12 -0700

    Completed newman conway problem

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -7,4 +7,34 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError
+
+    if num == 1:
+        return "1"
+
+    #make an array that is num spaces long 
+    sequence = [None] * (num + 1)
+
+    #Must set these up before coding the rest 
+    sequence[0] = 0
+    sequence[1] = 1
+    sequence[2] = 1
+
+    #now, I am starting at position 2 in the index 
+    index = 3
+    while index <= num:
+        sequence[index] = sequence[sequence[index-1]] + sequence[index-sequence[index-1]]
+        
+        index += 1
+    
+    string = ""
+    i = 1
+    while i < (len(sequence)):
+        string += str(sequence[i])
+        
+        if (len(sequence) - i) >= 2:
+            string += " "
+        i += 1
+    
+    return(string)
