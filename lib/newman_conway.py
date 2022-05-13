640851aefd4de11cdc6e15b708dcdc6952468279
Lux Barker <lux.barker13@gmail.com>
2022-05-12 17:03:08 -0700

    Implement newman-conway

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,21 @@
-
-
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+    """Returns a list of the Newman Conway numbers for the given value.
+    Time Complexity: ?
+    Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError
+
+    if num == 1:
+        return "1"
+
+    nums = [0, 1, 1]
+
+    i = 3
+    while i <= num:
+        nums.append(nums[nums[i - 1]] + nums[i - nums[i - 1]])
+        i += 1
+
+    return " ".join([str(n) for n in nums[1:]])
