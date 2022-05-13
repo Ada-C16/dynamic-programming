cf3c38805b345a1d3adf0b17517c38a581577ee2
Michelle Bodart <michelle.bodart@gmail.com>
2022-05-13 08:51:21 -0700

    more cleanup

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -5,14 +5,12 @@
 def newman_conway(num):
     if num < 1:
         raise(ValueError)
-    elif num == 1:
-        return '1'
-    elif num == 2:
-        return '1 1'
 
     memo = [None] * (num + 1)
-    memo[1] = 1
-    memo[2] = 1
+    if num >= 1:
+        memo[1] = 1
+    if num >= 2:
+        memo[2] = 1
 
     for i in range(3, num + 1):
         memo[i] = memo[memo[i - 1]] + memo[i - memo[i - 1]]
