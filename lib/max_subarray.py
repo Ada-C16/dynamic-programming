3b12f72aab15e4b0394f26b2859b664cabd47ad7
Stephanie Chen <stephanie.chenn1@gmail.com>
2022-05-15 19:31:20 -0400

    Passed all tests

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,20 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    else:
+        temp_sum = nums[0]
+        max_sum = max(nums)
+        for i in range(1,len(nums)):
+            if nums[i] > temp_sum + nums[i]:
+                temp_sum = nums[i]
+            else:
+                temp_sum = temp_sum + nums[i]
+            max_sum = max(max_sum, temp_sum)
+        return max_sum
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -7,4 +7,21 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num == 0:
+        raise ValueError("ValueError exception")
+
+    elif num  == 1:
+        return "1"
+    elif num == 2:
+        return "1 1"
+
+    output = "1 1"
+    memo = [0,1,1]
+
+    for i in range(3,num+1):
+        memo.append(memo[memo[i-1]] + memo[i-memo[i-1]])
+    
+    output = ""
+    for i in range(1,len(memo)):
+        output += str(memo[i]) + " "
+    return output.strip()
