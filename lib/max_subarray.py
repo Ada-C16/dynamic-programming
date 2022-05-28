7a91e388387e3917049e8b8f2e4d5c08bf9ddd6b
syblades <86990570+syblades@users.noreply.github.com>
2022-05-28 16:41:30 -0700

    10/10 tests passing

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,12 +2,10 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: 0(1)
     """
-    if nums == None:
-        return 0
-    if len(nums) == 0:
+    if nums == None or len(nums) == 0:
         return 0
     
     # building memo
