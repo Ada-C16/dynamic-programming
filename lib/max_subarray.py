17650740035616c6bb1340b5e737688ec847621e
Gabe Kelemen <gabe@gabekelemen.com>
2022-06-02 15:00:32 -0400

    Add newman_conway

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,8 +2,8 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: 0(n)
+        Space Complexity: 0(1)
     """
     
      # compare sum of current subarray as iterating thru array, remove subarrays with negative sum, keep track of max sum at each index
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,26 @@
 
-
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) because of loop
+        Space Complexity: O(n) beause storing previous values
     """
-    pass
+    # no Newman-Conway value for zero
+    # base case is for 1 and 2 and returns 1
+    
+    if num == 0:
+        raise ValueError("No Newman-Conway value for zero")
+
+    if num == 1:
+        return '1'
+    
+    memo = [0, 1, 1]
+    
+    # need to start range at index 3 after base cases
+    # loop thru to append value to memo for each index
+    for i in range(3, num + 1):
+        nc_value = memo[memo[i - 1]] + memo[i - memo[i - 1]]
+        memo.append(nc_value)
+
+    return ' '.join([str(item) for item in memo[1:]])
+    
+    
