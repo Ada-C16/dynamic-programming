d1212324d7448fa75fd8991328a7c42f8912d123
Khandice <s.khandice@gmail.com>
2022-06-07 13:58:50 -0700

    max_sub_array tests passing

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -9,4 +9,12 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+        
+    final_max = nums[0]
+    temp_max = nums[0]
+
+    for i in range(1, len(nums)):
+        temp_max = max(temp_max + nums[i], nums[i])
+        if temp_max > final_max:
+            final_max = temp_max
+    return final_max
