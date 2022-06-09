0298bf5b096c45f24033136db819c03ab131bf1b
Rachael <mcbride.rac@gmail.com>
2022-06-09 18:13:46 -0400

    Passes all max_subarray tests

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,21 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
-    if nums == None:
-        return 0
-    if len(nums) == 0:
-        return 0
-    pass
+    if not nums:
+        return 0 
+
+    max_so_far = 0
+    max_ending_here = 0
+
+    for num in nums:
+        max_ending_here = max_ending_here + num 
+        if max_ending_here < 0:
+            max_ending_here = 0
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+    
+    return max(nums) if max_so_far == 0 else max_so_far 
+
