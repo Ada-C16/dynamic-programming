e2b5726fe73a32b65101a0293a5772b51597d659
Rachael <mcbride.rac@gmail.com>
2022-06-09 18:05:32 -0400

    Passes newman_conway tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,15 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num <= 0:
+        raise ValueError 
+    elif num == 1:
+        return '1' 
+    else: 
+        memo = [0, 1, 1]
+        for i in range(3, num+1):
+            next_num = memo[memo[-1]] + memo[i-memo[i-1]]
+            memo.append(next_num)
+        return ' '.join([str(i) for i in memo if i])
