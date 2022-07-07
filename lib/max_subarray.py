8ea58b85a87e8c33d3592670a25eddeaf8c03418
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-07-07 15:25:39 -0400

    add complexity analysis and finish max sub array

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -1,12 +1,25 @@
 
+from json.encoder import INFINITY
+
+
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+
+    maximum = - INFINITY
+    current_max = 0
+
+    for num in nums:
+        current_max += num
+        if current_max > maximum:
+            maximum = current_max
+        if current_max < 0:
+            current_max = 0
+    return maximum
