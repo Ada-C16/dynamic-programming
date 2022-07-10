660da6150e4c024b7868f86997b270ec09a177ac
Katie <diazkatie@gmail.com>
2022-07-10 13:26:25 -0700

    passing tests for max_subarray and newman

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,17 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    maxSub = nums[0]
+    currentSum = nums[0]
+    for index in range(1, len(nums)):
+        currentSum = max(nums[index], currentSum + nums[index])
+        maxSub = max(maxSub, currentSum)
+    return maxSub
