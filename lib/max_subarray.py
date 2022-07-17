6de9cfc494e28f1cf7c01e1427ed43155dc1d3f0
Asli Athman <asathmao@amazon.com>
2022-07-17 16:25:24 -0400

    Adds time/space complexity info for max_sub_array

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,8 +2,8 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
     if nums == None:
         return 0
