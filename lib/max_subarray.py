c873f8d2de7b06c4d1e864d60c1997e762e78e9f
Angela Fan <Angela.Fan@oracle.com>
2022-07-18 10:53:10 -0700

    all tests passing

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -9,4 +9,28 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    if len(nums) == 1:
+        return nums[0]
+
+    # Initialize the max_sum to the first element in the list.
+    max_sum = nums[0]
+    # Initialize the current_sum to the first element in the list.
+    current_sum = nums[0]
+    # Iterate through the list.
+    for i in range(1, len(nums)):
+        # If the current sum is greater than 0, add the next element to it.
+        if current_sum > 0:
+            current_sum += nums[i]
+        # If the current sum is less than 0, set it to the next element.
+        elif current_sum < 0:
+            current_sum = nums[i]
+        # If the current sum is 0, set it to the next element.
+        elif current_sum == 0:
+            current_sum = nums[i]
+        # If the current sum is greater than the max sum, set the max sum to it.
+        if current_sum > max_sum:
+            max_sum = current_sum
+    # Return the max sum.
+    return max_sum
+
+
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,24 @@
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num == 0:
+        raise ValueError("Input must be greater than 0.")
+
+    if num == 1:
+        return "1"
+
+    if num == 2:
+        return "1 1"
+
+    # Initialize the list of numbers.
+    numbers = [1, 1]
+
+    for i in range(2, num + 1):
+        numbers.append(numbers[numbers[i - 1] - 1] + numbers[i - numbers[i - 1]])
+
+    # Return the list of numbers.
+    return " ".join(str(x) for x in numbers [0:num])
+    
