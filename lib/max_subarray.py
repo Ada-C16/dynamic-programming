dc9fbf7e360cf73c82198dddff477e2eb990814e
Emily Colon <emiquigs@gmail.com>
2022-07-18 19:15:28 -0400

    completed dynamic programming

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -7,6 +7,16 @@ def max_sub_array(nums):
     """
     if nums == None:
         return 0
+
     if len(nums) == 0:
         return 0
-    pass
+
+    max_til_now = nums[0]
+    max_ending = 0
+
+    for i in range(len(nums)):
+        max_ending += nums[i]
+        max_til_now = max(max_til_now, max_ending)
+        if(max_ending < 0):
+            max_ending = 0
+    return max_til_now
