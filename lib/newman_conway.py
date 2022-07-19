739b64905addd1233d60c75cf447c79f1bf9fac3
Juliana Ruiz <jruiz9312@gmail.com>
2022-07-18 21:41:14 -0700

    passing all waves

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,13 +1,32 @@
 
-
+import array
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: o(n)
+        Space Complexity: o(n)
     """
-    if num == 1 or num == 2: 
-        return 1 
-    else: 
-        return newman_conway(newman_conway(num - 1)) + newman_conway(num - newman_conway(num-1))
+    # to store values 
+    if num < 1: 
+        raise ValueError ("Invalid Num")
+    if num == 1: 
+        return "1"
+
+    f = [0, 1, 1]
+    output = "1"
+
+    count = 3 
+    # To store values of sequence in array
+
+    while count <= num: 
+        f.append(f[f[count - 1]]+ f[count - f [ count -1]])
+        count += 1 
+    number = [str(item) for item in f]
+    
+    return " ".join(number[1:])
+    # for i in range(3, num + 1):
+    #     r = f[f[i-1]]+f[i-f[i-1]]
+    #     f.append(r);
+ 
+    # return r
