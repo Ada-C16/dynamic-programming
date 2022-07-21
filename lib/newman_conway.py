28548fe9461a14d94b5e102a2d82c9f3a92b1ef5
Asli Athman <asliathman@gmail.com>
2022-07-21 18:11:54 -0400

    Answers newman_conway problem, passes all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,26 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    outputs = {}
+
+    res = []
+    if num <= 0:
+        raise ValueError
+
+    for n in range(1, num + 1):
+        if n == 1 or n ==2:
+            outputs[n] = 1
+            res.append("1")
+            continue
+
+        val = outputs[outputs[n-1]] + outputs[n - outputs[n - 1]]
+        outputs[n] = val
+        res.append(str(val))
+
+    return " ".join(res)
