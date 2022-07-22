f24335ac60a039d41de5cb4f9c0af03391bf20c4
evangeline spracklin <espracklin@dropbox.com>
2022-07-21 20:10:33 -0700

    base cases passing

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,6 @@ P(2) = 1
 for all n > 2
 P(n) = P(P(n - 1)) + P(n - P(n - 1)) 
 '''
-import array
 
 # Time complexity: ?
 # Space Complexity: ?
@@ -13,27 +12,13 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
+    # base case guard clauses
+    if num < 1:
+        raise ValueError('newman conway number cant be zero nums long')
+    elif num == 1:
+        return '1'
+    elif num == 2:
+        return '1 1'
+
+
     NCnums = [['i']]
-
-newman_conway(5)
-
-# ''' Python program to find the n-th element of
-#     Newman-Conway Sequence'''
-
-def sequence(n):
-    f = array.array('i', [0, 1, 1])
- 
-#     # To store values of sequence in array
-    for i in range(3, n + 1):
-        r = f[f[i-1]]+f[i-f[i-1]]
-        f.append(r);
- 
-#     return r
- 
-# # Driver code
-# def main():
-#     n = 10
-#     print sequence(n)
-     
-# if __name__ == '__main__':
-#     main()
