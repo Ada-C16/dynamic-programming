4d87ba88226d268f3316e479d6bf9543ffb5d944
evangeline spracklin <espracklin@dropbox.com>
2022-07-21 20:04:17 -0700

    max contiguous array

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,4 +1,10 @@
-
+''' 
+P(1) = 1
+P(2) = 1
+for all n > 2
+P(n) = P(P(n - 1)) + P(n - P(n - 1)) 
+'''
+import array
 
 # Time complexity: ?
 # Space Complexity: ?
@@ -7,4 +13,27 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    NCnums = [['i']]
+
+newman_conway(5)
+
+# ''' Python program to find the n-th element of
+#     Newman-Conway Sequence'''
+
+def sequence(n):
+    f = array.array('i', [0, 1, 1])
+ 
+#     # To store values of sequence in array
+    for i in range(3, n + 1):
+        r = f[f[i-1]]+f[i-f[i-1]]
+        f.append(r);
+ 
+#     return r
+ 
+# # Driver code
+# def main():
+#     n = 10
+#     print sequence(n)
+     
+# if __name__ == '__main__':
+#     main()
